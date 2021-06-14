9080e9edb8e839fc3b251f1e219d99b594f3a6a5
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-14 16:07:24 +0800

    Version 1.0

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let names = [
+        "John Smith",
+        "Dan Smith",
+        "Jason Smith",
+        "Mary Smith"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
 
 }
 
+//delegate function are used to handle interactions of cells
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("My name is", names[indexPath.row])
+    }
+}
+
+// need to prodive de function
+// 1 is NumberOfRowsInsection
+// 2 CellForRowAt
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        
+        cell.textLabel?.text = names[indexPath.row]
+        
+        return cell
+    }
+    
+}
